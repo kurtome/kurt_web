ea8a78d5e0c82738cfd400cfd6da696ffdd89e0b
kurtome <kurt@melby.me>
2019-07-25 16:40:46 -0400

    fixing mobile navigation

--- lib/kurt_web/views/page_view.ex
+++ lib/kurt_web/views/page_view.ex
@@ -1,11 +1,21 @@
 defmodule KurtWeb.PageView do
   use KurtWeb, :view
 
-  def selected_css_class(conn, path, css_class) do
+  def selected_css_class(conn, path, class) do
     request_path = URI.decode(conn.request_path)
 
     if request_path == path do
-      css_class
+      class
+    else
+      nil
+    end
+  end
+
+  def not_selected_css_class(conn, path, class) do
+    request_path = URI.decode(conn.request_path)
+
+    if request_path != path and request_path != "/" do
+      class
     else
       nil
     end
