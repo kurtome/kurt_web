d612a92e17c9517810271f1c91af08a76ab33ae8
kurtome <kurt@melby.me>
2019-07-25 10:21:16 -0400

    fixing purge css

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,17 +1,28 @@
 // postcss.config.js
-const purgecss = require('postcss-purgecss')({
+const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project 
   content: [
     '../lib/kurt_web/templates/**/*.html.eex',
-    '../lib/kurt_web/templates/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
+    '../lib/kurt_web/templates/**/*.html'
     // etc.
   ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  // // Include any special characters you're using in this regular expression
+  // extractors: [
+  //   {
+  //     extractor: class PurgeFromJs {
+  //       static extract(content) {
+  //         // return array of css selectors
+  //         return content.match(/[A-Za-z0-9-_:/]+/g) || []
+  //       }
+  //     },
+  //     extensions: ["html", "html.eex"]
+  //   }
+  // ]
+
 })
 
 module.exports = {
--- lib/kurt_web/views/page_view.ex
+++ lib/kurt_web/views/page_view.ex
@@ -1,11 +1,11 @@
 defmodule KurtWeb.PageView do
   use KurtWeb, :view
 
-  def selected_css_class(conn, path) do
+  def selected_css_class(conn, path, css_class) do
     request_path = URI.decode(conn.request_path)
 
     if request_path == path do
-      "selected"
+      css_class
     else
       nil
     end
