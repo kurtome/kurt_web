134311ec837e749c209fee56146eed005c197e77
kurtome <kurt@melby.me>
2019-07-25 15:27:16 -0400

    fixing ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kurt, KurtWeb.Endpoint,
-  url: [host: "kurt.melby.me", port: 80],
-  force_ssl: [hsts: true, host: "kurt.melby.me"],
+  url: [host: "kurt.melby.me", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
