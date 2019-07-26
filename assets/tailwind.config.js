768c3edf4239b34652a8ac10da501abe83fafee8
kurtome <kurt@melby.me>
2019-07-26 12:46:42 -0400

    styling tweaks

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       'ui': ['Averia Serif Libre', 'serif'],
       'body': ['Roboto Slab', 'sans-serif'],
       'code': ['Roboto Mono', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'black-a25': 'rgba(0, 0, 0, 0.25)',
+        'black-a50': 'rgba(0, 0, 0, 0.5)',
+        'black-a75': 'rgba(0, 0, 0, 0.75)'
+      }
     }
   },
   variants: {},
